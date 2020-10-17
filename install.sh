9f53040133d8b94e2f31e7d6a44d56dcb55253a4
jijie <jijie@someplace.com>
2020-10-17 22:43:14 +0800

    先实现官方文档中的指引

--- install.sh
+++ install.sh
@@ -8,6 +8,8 @@ set -x
     # remote 1
     # remote 2
 
+# import cacert manually
+
 CONTROL_PLANE_CTX=$1
 DATA_PLAINE_CTXS=$2
 
@@ -27,20 +29,20 @@ kubectl apply -f ./multicluster-aware-gateway.yaml -n $ISTIO_NAMESPACE --context
 ISTIOD_REMOTE_EP=
 until [ ! -z "$ISTIOD_REMOTE_EP" ] ; do
     echo "Trying to get a public IP for ingressgateway at ctx-${CONTROL_PLANE_CTX}..."
-    ISTIOD_REMOTE_EP=$(kubectl get svc istio-ingressgateway -n $ISTIO_NAMESPACE --context=ctx-${CONTROL_PLANE_CTX} -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+    ISTIOD_REMOTE_EP=$(kubectl get svc/istio-ingressgateway -n $ISTIO_NAMESPACE --context=ctx-${CONTROL_PLANE_CTX} -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
     sleep 1.5;
 done
 
 for DATA_PLANE in "${DATA_PLAINE_NAMES[@]}" ; do
-    sed "s/ISTIOD_REMOTE_EP/$ISTIOD_REMOTE_EP/" ./data-plane.yaml | sed "s/DATA_PLANE_NAME/$DATA_PLANE/" > ./.install/data-plne-$DATA_PLANE.yaml
+    sed "s/ISTIOD_REMOTE_EP/$ISTIOD_REMOTE_EP/" ./data-plane.yaml | sed "s/DATA_PLANE_NAME/$DATA_PLANE/" > ./.install/data-plne-$DATA_PLANE-values.yaml
 
-    kubectl apply -f ./.install/control-plane-ca-cm.yaml -n $ISTIO_NAMESPACE --context=ctx-${DATA_PLANE}
-    istioctl install -f ./.install/data-plne-$DATA_PLANE.yaml -n $ISTIO_NAMESPACE --context=ctx-${DATA_PLANE}
+    istioctl manifest generate -f ./.install/data-plne-$DATA_PLANE.yaml -n $ISTIO_NAMESPACE > ./.install/data-plne-$DATA_PLANE-install.yaml
     
+    kubectl apply -f ./.install/data-plne-$DATA_PLANE-install.yaml  -n $ISTIO_NAMESPACE --context=ctx-${DATA_PLANE} 
+
     kubectl apply -f ./multicluster-aware-gateway.yaml -n $ISTIO_NAMESPACE --context=ctx-${DATA_PLANE} 
     istioctl x create-remote-secret --name ${DATA_PLANE} -n $ISTIO_NAMESPACE --context=ctx-${DATA_PLANE} | \
         kubectl apply -n $ISTIO_NAMESPACE --context=ctx-${CONTROL_PLANE_CTX} -f -
+
+    kubectl rollout status deployment/istio-ingressgateway
 done
-
-
- 
